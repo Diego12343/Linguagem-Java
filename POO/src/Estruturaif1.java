ebe5767571e3758f30a4bdfb98e949a0d3bf09a9
Diego Gomes <diego@yahoo.com.br>
2023-03-14 09:15:49 -0300

    Programacao Orientada a Objeto e Estruturas

---
+++ POO/src/Estruturaif1.java
@@ -0,0 +1,13 @@
+package br.com.poo.estruturaif;
+
+public class Estruturaif1 {
+
+	public static void main(String[] args) {
+		System.out.println("========== Valor Par ou Impar=========")
+        System.out.println("\nEntre com um valor numérico");
+		//declaração da variável numérica
+		int numero;
+		Scanner entra= new Scanner
+	}
+
+}
