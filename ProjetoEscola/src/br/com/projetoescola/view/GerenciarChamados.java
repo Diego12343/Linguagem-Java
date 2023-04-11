66db50cbffbba52bcfcc5575d5c42c767f0e9175
Diego Gomes <diego@yahoo.com.br>
2023-04-11 09:57:08 -0300

    Projeto Escola

---
+++ ProjetoEscola/src/br/com/projetoescola/view/GerenciarChamados.java
@@ -0,0 +1,43 @@
+package br.com.projetoescola.view;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+
+public class GerenciarChamados extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GerenciarChamados frame = new GerenciarChamados();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GerenciarChamados() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 666, 444);
+		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 128, 255));
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+
+}
